4517c89f6c43cd999bcfd9750873a38a1cb5a701
Scott Brunswig <sbrunswig@gmail.com>
2019-12-05 16:26:50 -0600

    update dist

--- dist/js/about.19113a1c.js
+++ dist/js/about.19113a1c.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"91aa":function(e,t,n){},e76e:function(e,t,n){"use strict";var a=n("91aa"),s=n.n(a);s.a},f820:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an page")])])}],u=(n("e76e"),n("2877")),c={},i=Object(u["a"])(c,a,s,!1,null,null,null);t["default"]=i.exports}}]);
-//# sourceMappingURL=about.1c2b06e5.js.map
+//# sourceMappingURL=about.19113a1c.js.map
